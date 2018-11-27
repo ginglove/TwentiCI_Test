176c8fb14c7c8f7387d2c5b1ee15c6b46c9c562e
Truong Giang Le <truonggiangle91@gmail.com>
2018-11-27 18:38:10 +0700

    Create twentyci_Test1
    Commit twentyci_Test1

---
+++ Twentyci_Test1/Twentyci_Test_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twentyci_Test_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            string test = Checkreverse_Text("This is another test");
+            Console.WriteLine(test);
+            Console.ReadLine();
+        }
+        public static string Checkreverse_Text(string Input)
+        {
+            char[] whitespace = new char[] { ' ', '\t' };
+            string[] strNewInput = Input.Split(whitespace);
+            string result = "";
+            for (int i = 0; i < strNewInput.Length; i++)
+            {
+                if (strNewInput[i].Length > 4)
+                {
+                    strNewInput[i] = Reverse(strNewInput[i]);
+                }
+                result += " " + strNewInput[i];
+            }
+            return result;
+        }
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
